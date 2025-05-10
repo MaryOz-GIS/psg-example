da8120970bed425736b1ca1aa49cb855867d760b
MaryOz-GIS <maria.oscori@gmail.com>
2025-05-10 00:43:20 -0400

    Conversacon de la Karen y la Negrita

---
+++ reto_sesion_03/ejercicio_03.py
@@ -0,0 +1,24 @@
+print("TUTORIAL PARA LAVAR PLATOS NIVEL VIVIENDO SOLOS")
+print("MATERIALES")
+print("fregadero o recipiente grande, agua caliente,esponja,jabon de lavar platos,secador de tela y platos sucios!!!"")
+
+print("1.PREPARACIOn")
+print("Llena el fregadero o un recipiente grande con agua caliente, suficiente para sumergir los platos")
+
+print("2.AGREGA JABON")
+print("Añade un poco de jabón para lavar platos al agua caliente")
+
+print("3.RASPA LOS RESTOS DE COMIDA")
+print("Añade un poco de jabón para lavar platos al agua caliente")
+
+print("4.LAVA LOS PLATOS")
+print("Sumerge los platos en el agua jabonosa y frégalos con una esponja")
+
+print("5.ENJUAGUE")
+print("Enjuaga bien los platos con agua caliente para eliminar cualquier residuo de jabón y comida")
+
+print("6.SECADO")
+print("Deja que los platos se sequen al aire sobre un escurreplatos o sécalos con un paño limpio")
+
+print("ALMACENAMIENTO")
+print("Una vez que los platos estén secos, guárdalos en un lugar limpio y seguro")
